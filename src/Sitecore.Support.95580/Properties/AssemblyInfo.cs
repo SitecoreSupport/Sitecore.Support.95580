1ee32f1790e8d636efc5d28ade2d96f7740c526c
DK\yho <yho@sitecore.net>
2017-10-02 18:01:30 +0800

    Commit patch

---
+++ src/Sitecore.Support.95580/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95580")]
+[assembly: AssemblyProduct("Sitecore.Support.95580")]
+[assembly: ComVisible(false)]
